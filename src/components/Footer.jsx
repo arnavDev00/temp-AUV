72a5262a7d5ae7a2be75769d8a301d20b6ce3202
arnavDev00 <bgvkj00@gmail.com>
2021-12-20 09:48:12 +0530

    first

---
+++ src/components/Footer.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <React.Fragment>
+      <div className="footer">
+        <p className="p-3">
+          Made with love {"        "}
+          <i id="heart" className="fa">
+            &#xf004;
+          </i>
+          {"         "}
+          by AUV IIITDM . Copyright © 2020. All Rights Reserved.
+          <a href="https://www.privacypolicies.com/live/af693fba-7751-41fb-b826-ac1036dd0e85">
+            {"         "} Read our Privacy Policy
+          </a>
+        </p>
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default Footer;
